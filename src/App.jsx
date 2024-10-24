854b9080c0743b3e916f09dcf0244b4faa7e52a1
Vijayyy <112235237+Vijayyywho@users.noreply.github.com>
2024-10-24 16:46:09 +0530

    app

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,11 @@ import React, { useEffect } from "react";
 import { useAuth0 } from "@auth0/auth0-react";
 import { message } from "antd"; // Import the message component from Ant Design
 import HomePage from "./routes/HomePage";
-import "./index.css"; // or wherever your Tailwind styles are
 import ListPage from "./routes/ListPage/ListPage";
 import Layout from "./routes/layout/Layout";
 import SinglePage from "./routes/Single/SinglePage";
 import Profile from "./Components/Profile/Profile";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import { HashRouter, Route, Routes } from "react-router-dom"; // Use HashRouter
 
 function App() {
   const { loginWithRedirect, isAuthenticated, user } = useAuth0();
@@ -20,45 +18,32 @@ function App() {
     }
   }, [isAuthenticated, user]);
 
-  // const router = createBrowserRouter([
-  //   {
-  //     path: "/",
-  //     element: <Layout />,
-  //     children: [
-  //       {
-  //         path: "/",
-  //         element: <HomePage />,
-  //       },
-  //       {
-  //         path: "/list",
-  //         element: <ListPage />,
-  //       },
-  //       {
-  //         path: "/:id",
-  //         element: <SinglePage />,
-  //       },
-  //       {
-  //         path: "/profile",
-  //         element: <Profile />,
-  //       },
-  //     ],
-  //   },
-  // ]);
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <Layout />,
+      children: [
+        {
+          path: "/",
+          element: <HomePage />,
+        },
+        {
+          path: "/list",
+          element: <ListPage />,
+        },
+        {
+          path: "/:id",
+          element: <SinglePage />,
+        },
+        {
+          path: "/profile",
+          element: <Profile />,
+        },
+      ],
+    },
+  ]);
 
-  return (
-    <HashRouter>
-      {" "}
-      {/* Use HashRouter to ensure compatibility with static hosting */}
-      <Layout>
-        <Routes>
-          <Route path="/" element={<HomePage />} />
-          <Route path="/list" element={<ListPage />} />
-          <Route path="/:id" element={<SinglePage />} />
-          <Route path="/profile" element={<Profile />} />
-        </Routes>
-      </Layout>
-    </HashRouter>
-  );
+  return <RouterProvider router={router} />;
 }
 
 export default App;
