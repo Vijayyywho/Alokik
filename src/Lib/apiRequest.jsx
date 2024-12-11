46aa4c94f895e77a1ae417d6ad7c821ce7ccb924
Vijayyy <112235237+Vijayyywho@users.noreply.github.com>
2024-12-11 12:40:24 +0530

    gitt

--- src/Lib/apiRequest.jsx
+++ src/Lib/apiRequest.jsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const apiRequest = axios.create({
-  baseURL: "https://alokikapiiii.onrender.com/.com/api", // Replace with your production API URL
+  baseURL: "https://alokikapiiii.onrender.com/api", // Replace with your production API URL
   withCredentials: true, // If you need to send cookies
 });
 
