dfcd904acb2ac23736d350de58933a34daed8aef
Vijayyy <112235237+Vijayyywho@users.noreply.github.com>
2024-12-11 12:38:24 +0530

    api

--- src/Lib/apiRequest.jsx
+++ src/Lib/apiRequest.jsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const apiRequest = axios.create({
-  baseURL: "http://localhost:3000/api",
+  baseURL: "https://alokikapiiii.onrender.com/.com/api", // Replace with your production API URL
   withCredentials: true, // If you need to send cookies
 });
 
